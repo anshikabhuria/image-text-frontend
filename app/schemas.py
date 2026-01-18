85f9b9701867cba9d777f8959e2985c149725e6d
Manas Das <noragami0021@gmail.com>
2026-01-18 12:42:12 +0530

    updates schema to return nlp output

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from typing import List
+
+
+class NLPResult(BaseModel):
+    targets: List[str]
+    protected: List[str]
+
 
 class UploadResponse(BaseModel):
     id: int
     text: str
     image_path: str
+    nlp: NLPResult
